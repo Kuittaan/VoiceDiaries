ce74908ddf38cc86fbdfb8a2b676759e2ffbbbb5
Kuitta <Kuitti.anne@gmail.com>
2024-02-08 02:15:01 +0200

    Implement deleting all entries, uriToBitmap function

--- app/src/main/java/org/kuittaan/voicediary/view/SettingsView.kt
+++ app/src/main/java/org/kuittaan/voicediary/view/SettingsView.kt
@@ -1,95 +1,119 @@
 package org.kuittaan.voicediary.view
 
+import android.content.ContentResolver
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
-import org.kuittaan.voicediary.model.EntryDatabase
 import org.kuittaan.voicediary.viewmodel.EntryRepository
+import java.io.FileDescriptor
+import java.io.IOException
 
 class SettingsView: ViewModel() {
 
+    private lateinit var applicationContext: Context // Assuming you set this in your application or wherever appropriate
+
+    private val sharedPreferences by lazy {
+        applicationContext.getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+    }
+
     @Composable
-    fun createSettingsView(entryRepository: EntryRepository) {
+    fun CreateSettingsView(entryRepository: EntryRepository) {
 
-        Column {
+        Box {
+            val uri: Uri? by rememberUpdatedState(loadImageFromGallery())
 
-            // todo: set navigation visual
-
-            var checked by remember { (mutableStateOf(true)) }
-            Switch(
-                checked = checked, onCheckedChange = {checked = it
-                }
+            AsyncImage(
+                model = uri,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Crop,
             )
 
-            Button(onClick = {
-                // todo: add warning/confirmation
-                viewModelScope.launch {
-                    entryRepository.deleteAllEntries()
+            Column {
+                // todo: set navigation visual
+                var checked by remember { (mutableStateOf(true)) }
+                Switch(
+                    checked = checked, onCheckedChange = {
+                        checked = it
+                    }
+                )
+
+                Button(onClick = {
+                    // todo: add warning/confirmation
+                    viewModelScope.launch {
+                        entryRepository.deleteAllEntries()
+                    }
+                }) {
+                    Text(text = "Delete all entries")
                 }
-            }) {
-                Text(text = "Delete all entries")
+
+                if (checked) {
+                }
             }
-
-            loadImageFromGallery()
-            if (checked) {
-
-            }
-
         }
     }
 
     @Composable
-    fun loadImageFromGallery() {
+    fun loadImageFromGallery(): Uri? {
         var imageUri by remember {
             mutableStateOf<Uri?>(null)
         }
+
         val launcher = rememberLauncherForActivityResult(
             contract = ActivityResultContracts.GetContent()
         ) { uri: Uri? ->
-            imageUri = uri
+            // Save the selected image URI to SharedPreferences
+            uri?.let {
+                imageUri = it
+            }
         }
+
+        fun uriToBitmap(selectedFileUri: Uri): Bitmap? {
+            try {
+                lateinit var contentResolver: ContentResolver
+                val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedFileUri, "r")
+                val fileDescriptor: FileDescriptor = parcelFileDescriptor!!.fileDescriptor
+                val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                parcelFileDescriptor.close()
+                return image
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
         Button(onClick = {
+            // launch gallery selection only for images
             launcher.launch("image/*")
         }) {
             Text(text = "Set background image")
         }
-        AsyncImage(
-            model = imageUri,
-            contentDescription = null,
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxHeight()
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(12.dp)),
-            contentScale = ContentScale.Crop,
-        )
 
-
+        return imageUri
     }
 }
