ffb15f0573b2c9cb1159861338eed547f7391397
Kuitta <Kuitti.anne@gmail.com>
2024-02-17 13:22:11 +0200

    update getting voice result

--- app/src/main/java/org/kuittaan/voicediary/model/STTManager.kt
+++ app/src/main/java/org/kuittaan/voicediary/model/STTManager.kt
@@ -6,51 +6,55 @@ import android.os.Bundle
 import android.speech.SpeechRecognizer
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import java.util.Locale
 
 // TODO: handle RECORD_AUDIO permission
 class STTManager(context: Context) {
 
-    // TODO: Update to API31 from API30 to use createOnDeviceSpeechRecognizer instead
     private val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context)
     private val recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
     private val recognitionListener = object : RecognitionListener {
         override fun onReadyForSpeech(p0: Bundle?) {
-            TODO("Not yet implemented")
         }
 
         override fun onBeginningOfSpeech() {
-            TODO("Not yet implemented")
+            // todo: display start of speaking in ui
         }
 
         override fun onRmsChanged(p0: Float) {
-            TODO("Not yet implemented")
         }
 
         override fun onBufferReceived(p0: ByteArray?) {
-            TODO("Not yet implemented")
         }
 
         override fun onEndOfSpeech() {
-            TODO("Not yet implemented")
+            // todo: display end of speech in ui
         }
 
-        override fun onError(p0: Int) {
-            TODO("Not yet implemented")
+        override fun onError(error: Int) {
+            Log.e("STT", "error: $error")
         }
 
         override fun onResults(results: Bundle?) {
-            val recognizedText = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.get(0)
-             //TODO: Do something with the final recognized text
+            val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            if(!matches.isNullOrEmpty()) {
+                val text = matches[0]
+                // todo: display text in ui
+            }
         }
 
-        override fun onPartialResults(p0: Bundle?) {
-            TODO("Not yet implemented")
+        override fun onPartialResults(results: Bundle?) {
+            val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            if (!matches.isNullOrEmpty()) {
+                val text = matches[0]
+                // todo: display text in ui
+            }
         }
 
         override fun onEvent(p0: Int, p1: Bundle?) {
-            TODO("Not yet implemented")
         }
     }
 
