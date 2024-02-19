d8c76570b26ee164bcb3751751d9324a8a5fbcf7
Kuitta <Kuitti.anne@gmail.com>
2024-02-19 23:36:18 +0200

    implement singleton

--- app/src/main/java/org/kuittaan/voicediary/model/STTManager.kt
+++ app/src/main/java/org/kuittaan/voicediary/model/STTManager.kt
@@ -12,56 +12,58 @@ import androidx.core.content.ContentProviderCompat.requireContext
 import java.util.Locale
 
 // TODO: handle RECORD_AUDIO permission
-class STTManager() {
+class STTManager {
 
-    fun startVoiceInput(context: Context) {
-        // todo: ask for permission
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Recording voice ...")
+    companion object {
+        fun startVoiceInput(context: Context) {
+            // todo: ask for permission
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Recording voice ...")
 
-        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context)
-        speechRecognizer?.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(p0: Bundle?) {
-            }
-
-            override fun onBeginningOfSpeech() {
-                // todo: display start of speaking in ui
-            }
-
-            override fun onRmsChanged(p0: Float) {
-            }
-
-            override fun onBufferReceived(p0: ByteArray?) {
-            }
-
-            override fun onEndOfSpeech() {
-                // todo: display end of speech in ui
-            }
-
-            override fun onError(error: Int) {
-                Log.e("STT", "error: $error")
-            }
-
-            override fun onResults(results: Bundle?) {
-                val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                if(!matches.isNullOrEmpty()) {
-                    val text = matches[0]
-                    // todo: display text in ui
+            val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context)
+            speechRecognizer?.setRecognitionListener(object : RecognitionListener {
+                override fun onReadyForSpeech(p0: Bundle?) {
                 }
-            }
 
-            override fun onPartialResults(results: Bundle?) {
-                val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                if (!matches.isNullOrEmpty()) {
-                    val text = matches[0]
-                    // todo: display text in ui
+                override fun onBeginningOfSpeech() {
+                    // todo: display start of speaking in ui
                 }
-            }
 
-            override fun onEvent(p0: Int, p1: Bundle?) {
-            }
-        })
+                override fun onRmsChanged(p0: Float) {
+                }
+
+                override fun onBufferReceived(p0: ByteArray?) {
+                }
+
+                override fun onEndOfSpeech() {
+                    // todo: display end of speech in ui
+                }
+
+                override fun onError(error: Int) {
+                    Log.e("STT", "error: $error")
+                }
+
+                override fun onResults(results: Bundle?) {
+                    val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                    if(!matches.isNullOrEmpty()) {
+                        val text = matches[0]
+                        // todo: display text in ui
+                    }
+                }
+
+                override fun onPartialResults(results: Bundle?) {
+                    val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                    if (!matches.isNullOrEmpty()) {
+                        val text = matches[0]
+                        // todo: display text in ui
+                    }
+                }
+
+                override fun onEvent(p0: Int, p1: Bundle?) {
+                }
+            })
+        }
     }
 }
